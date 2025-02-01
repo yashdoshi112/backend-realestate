f2881a73cc4ba1f182a71d1c461580d3a07f03c9
yashdoshi112 <yashdoshi112@gmail.com>
2025-02-02 02:51:31 +0530

    change

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const pool = new Pool({
   database: 'postgres',
   password: 'yash_doshi01',
   port: process.env.PORT || 5000,
-  connectionString: process.env.DATABASE_URL || 'postgres://ubgashlj4v1c63:pbeb17504ba6e7f0715e6a16e2cf6e73ed67c6883eb8c9a8fec90fc01aede6fd0@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2s966es8qreve'
+  connectionString: process.env.DATABASE_URL || 'postgres://ubgashlj4v1c63:pbeb17504ba6e7f0715e6a16e2cf6e73ed67c6883eb8c9a8fec90fc01aede6fd0@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2s966es8qreve',
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
