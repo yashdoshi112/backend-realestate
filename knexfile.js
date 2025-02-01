f6273435615d327ac3971cb84b19e6cd3cf8518a
yashdoshi112 <yashdoshi112@gmail.com>
2025-02-02 03:30:21 +0530

    change

--- knexfile.js
+++ knexfile.js
@@ -7,14 +7,13 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './migrations'  // Ensure this is the correct relative path
+      directory: './migrations'
     },
     ssl: { rejectUnauthorized: false }
   },
-
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgres://ubgashlj4v1c63:pbeb17504ba6e7f0715e6a16e2cf6e73ed67c6883eb8c9a8fec90fc01aede6fd0@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2s966es8qreve',
+    connection: process.env.DATABASE_URL || 'postgres://postgres:yash_doshi01@localhost:5432/postgres',
     migrations: {
       directory: './migrations'
     }
