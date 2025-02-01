ede01d7a69197137d47ea2f5b51f8c129d7f2c52
yashdoshi112 <yashdoshi112@gmail.com>
2025-02-02 02:42:10 +0530

    change

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'postgres',
   password: 'yash_doshi01',
-  port: 5432,
+  port: process.env.PORT || 5000,
   connectionString: process.env.DATABASE_URL || 'postgres://ubgashlj4v1c63:pbeb17504ba6e7f0715e6a16e2cf6e73ed67c6883eb8c9a8fec90fc01aede6fd0@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d2s966es8qreve'
 });
 
