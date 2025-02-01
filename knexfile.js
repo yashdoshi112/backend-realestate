5e2754f34b71acc765f14004d35811e83ba79715
yashdoshi112 <yashdoshi112@gmail.com>
2025-02-02 03:10:19 +0530

    change

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './migrations'
+      directory: 'migrations'
     },
     ssl: { rejectUnauthorized: false }
   },
